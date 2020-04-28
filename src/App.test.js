d5fc5ac843de9cb4cde09f81ceacec22cbeae62b
Jayfromp <jcompson@hotmail.co.uk>
2020-04-28 13:16:13 +0100

    create and test basic functional header component.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Header component", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const Header = getByText(/Header/i);
+  expect(Header).toBeInTheDocument();
 });
